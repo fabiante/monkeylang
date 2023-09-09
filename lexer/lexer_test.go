14c50462cc5748349f2942852363f184b3ab8d5e
fabiante <fabiante@users.noreply.github.com>
2023-09-09 15:56:58 +0200

    Add lexer test for small program

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -37,4 +37,69 @@ func TestLexer_NextToken(t *testing.T) {
 			assert.Equal(t, test.expectedType, actual.Type, "unexpected token type %d", i)
 		}
 	})
+
+	t.Run("small program", func(t *testing.T) {
+		input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+    x + y;
+};
+
+let result = add(five, ten);`
+
+		tests := []struct {
+			expectedType    token.TokenType
+			expectedLiteral string
+		}{
+			{token.Let, "let"},
+			{token.Identifier, "five"},
+			{token.Assign, "="},
+			{token.Int, "5"},
+			{token.Semicolon, ";"},
+			{token.Let, "let"},
+			{token.Identifier, "ten"},
+			{token.Assign, "="},
+			{token.Int, "10"},
+			{token.Semicolon, ";"},
+			{token.Let, "let"},
+			{token.Identifier, "add"},
+			{token.Assign, "="},
+			{token.Func, "fn"},
+			{token.LParen, "("},
+			{token.Identifier, "x"},
+			{token.Comma, ","},
+			{token.Identifier, "y"},
+			{token.RParen, ")"},
+			{token.LBrace, "{"},
+			{token.Identifier, "x"},
+			{token.Plus, "+"},
+			{token.Identifier, "y"},
+			{token.Semicolon, ";"},
+			{token.RBrace, "}"},
+			{token.Semicolon, ";"},
+			{token.Let, "let"},
+			{token.Identifier, "result"},
+			{token.Assign, "="},
+			{token.Identifier, "add"},
+			{token.LParen, "("},
+			{token.Identifier, "five"},
+			{token.Comma, ","},
+			{token.Identifier, "ten"},
+			{token.RParen, ")"},
+			{token.Semicolon, ";"},
+			{token.EOF, ""},
+		}
+
+		lexer := NewLexer(input)
+
+		for i, test := range tests {
+			actual, err := lexer.NextToken()
+			require.NoError(t, err, "error when parsing token %d", i)
+			require.NotNil(t, actual, "parsing token %d returned nil", i)
+
+			assert.Equal(t, test.expectedLiteral, actual.Literal, "unexpected token literal %d", i)
+			assert.Equal(t, test.expectedType, actual.Type, "unexpected token type %d", i)
+		}
+	})
 }
