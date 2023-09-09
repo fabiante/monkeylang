8b68c6ec504c2330c8faae33b7bfe720007b7248
fabiante <fabiante@users.noreply.github.com>
2023-09-09 15:50:22 +0200

    Add identifier lookup

--- token/token.go
+++ token/token.go
@@ -5,6 +5,8 @@ type TokenType int
 const (
 	Illegal TokenType = iota
 	EOF
+	// Identifier is a user-defined identifier. This is the opposite
+	// from keywords of the language.
 	Identifier
 	Int
 	Assign
@@ -22,4 +24,16 @@ const (
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"let": Let,
+	"fn":  Func,
+}
+
+func LookupIdentifier(literal string) TokenType {
+	if t, ok := keywords[literal]; ok {
+		return t
+	}
+	return Identifier
 }
