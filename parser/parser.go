6dc4a3ebeeac79f80f7a4420c150b5bebea7efa8
fabiante <fabiante@users.noreply.github.com>
2023-09-10 11:13:52 +0200

    Implement parsing of let statements
    This is not fully finished since the expression is not yet parsed

--- parser/parser.go
+++ parser/parser.go
@@ -21,11 +21,80 @@ func NewParser(lexer *lexer.Lexer) *Parser {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	panic("not implemented")
+	prog := ast.NewProgram()
+
+	for !p.currTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			prog.Statements = append(prog.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return prog
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.token.Type {
+	case token.Let:
+		return p.parseLetStatement()
+	default:
+		return nil // TODO: error?
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{
+		Token: p.token,
+		Name:  nil,
+		Value: nil,
+	}
+
+	if !p.expectPeek(token.Identifier) {
+		return nil // TODO: error?
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.token,
+		Value: p.token.Literal,
+	}
+
+	if !p.expectPeek(token.Assign) {
+		return nil // TODO: error?
+	}
+
+	// TODO: Parse expression instead of ignoring it and skipping to semicolon
+	for !p.currTokenIs(token.Semicolon) {
+		p.nextToken()
+	}
+
+	return stmt
 }
 
 func (p *Parser) nextToken() {
 	p.token = p.peekToken
 	peek := p.lexer.NextToken()
 	p.peekToken = peek
+}
+
+// expectPeek checks if the next token is of the given type,
+//
+// If it is, it will advance to the next token by calling nextToken and return true.
+//
+// If it isn't, it will return false.
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) currTokenIs(t token.TokenType) bool {
+	return p.token.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
 }
