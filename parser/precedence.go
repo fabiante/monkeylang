88034e41b54476e8e08b24ef901b3e74b304ef68
fabiante <fabiante@users.noreply.github.com>
2023-09-11 09:25:20 +0200

    Add TokenType to precedence mapping

--- parser/precedence.go
+++ parser/precedence.go
@@ -1,5 +1,7 @@
 package parser
 
+import "github.com/fabiante/monkeylang/token"
+
 type precedence int
 
 const (
@@ -12,3 +14,16 @@ const (
 	prefix
 	call
 )
+
+// precedences maps token types to their precedence, used by Parser when parsing
+// expressions.
+var precedences = map[token.TokenType]precedence{
+	token.EQ:       equals,
+	token.NEQ:      equals,
+	token.LT:       lessgreater,
+	token.GT:       lessgreater,
+	token.Plus:     sum,
+	token.Minus:    sum,
+	token.Slash:    product,
+	token.Asterisk: product,
+}
